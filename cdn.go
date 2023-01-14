39069677a4bef0ee931d61a857c5266252ad9f3f
Sandeep Kumar <sandeepqmar05@gmail.com>
2023-01-14 15:48:37 +0530

    Added /get and /save endpoints

---
+++ cdn.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	//"github.com/qiniu/qmgo"
+	"mime/multipart"
+
+	"github.com/davidbyttow/govips/v2/vips"
+	"github.com/gernest/alien"
+	"github.com/google/uuid"
+)
+
+func main() {
+	log.Println("Initializing the router!")
+	r := alien.New()
+	vips.Startup(&vips.Config{MaxCacheMem: (256 << 20)})
+	r.Post("/save", handleSave)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("{message: \"Use /save or /get\"}"))
+	})
+	r.Get("/get", handleGet)
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 10 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
+}
+
+const SAVE_PATH = "images/"
+
+func handleSave(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello"))
+	imageData, imageHeader, err := r.FormFile("image")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("{error: \"Failure while allocating file\", fullError: \"" + err.Error() + "\" }"))
+		log.Panicln(err)
+	}
+	err = saveImage(imageData, imageHeader, SAVE_PATH)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("{error: \"Failure while saving to file\", fullError: \"" + err.Error() + "\" }"))
+		log.Panicln(err)
+	}
+	w.Write([]byte("{message: \"Success\"}"))
+}
+
+func saveImage(data io.Reader, header *multipart.FileHeader, savePath string) error {
+	log.Println("Reading the file")
+	image, err := vips.NewImageFromReader(data)
+	if err != nil {
+		return err
+	}
+	image.OptimizeICCProfile()
+	if err != nil {
+		return err
+	}
+	log.Println("Starting the export")
+	imageData, imageMeta, err := image.ExportAvif(&vips.AvifExportParams{StripMetadata: true, Quality: 90, Speed: 1})
+	if err != nil {
+		return err
+	}
+	log.Println("Writing the file")
+	err = ioutil.WriteFile(savePath+"Image-"+uuid.NewString()+imageMeta.Format.FileExt(), imageData, 0644)
+	if err != nil {
+		return err
+	}
+	uuid.NewString()
+	return nil
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	requiredFields := []string{"uuid", "width", "height"}
+	for i := 0; i < len(requiredFields); i++ {
+		if !q.Has(requiredFields[i]) {
+			handleErr(w, http.StatusBadRequest, "Missing Fields", errors.New("Missing Fields: "+requiredFields[i]))
+			return
+		}
+		imageId, err := uuid.Parse(q["uuid"][0])
+		if err != nil {
+			handleErr(w, http.StatusBadRequest, "Invalid UUID", err)
+			return
+		}
+		imageName := SAVE_PATH + "Image-" + imageId.String() + ".avif"
+		image, err := vips.NewImageFromFile(imageName)
+		if err != nil {
+			handleErr(w, http.StatusInternalServerError, "Couldn't open file", err)
+			return
+		}
+		width, err := strconv.Atoi(q["width"][0])
+		if err != nil {
+			handleErr(w, http.StatusBadRequest, "Invalid width", err)
+			return
+		}
+		height, err := strconv.Atoi(q["height"][0])
+		if err != nil {
+			handleErr(w, http.StatusBadRequest, "Invalid height", err)
+			return
+		}
+		err = image.Thumbnail(width, height, vips.InterestingCentre)
+		if err != nil {
+			handleErr(w, http.StatusBadRequest, "Couldn't Resize Image", err)
+			return
+		}
+		imageData, _, err := image.ExportAvif(&vips.AvifExportParams{StripMetadata: true, Quality: 80, Speed: 2})
+		if err != nil {
+			handleErr(w, http.StatusInternalServerError, "Couldn't Encode Image", err)
+			return
+		}
+		w.Header().Set("Content-Type", "image/avif")
+		w.Write(imageData)
+
+	}
+}
+
+func handleErr(w http.ResponseWriter, code int, message string, err error) {
+	w.WriteHeader(code)
+	w.Write([]byte(fmt.Sprintf("{error: [\"%s: %s\", \"%s\"]}", http.StatusText(code), message, err.Error())))
+}
+
+func isImage(header *multipart.FileHeader) bool {
+	return header.Header.Get("Content-Type")[0:6] == "image/"
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/Techfest-Sliet/image
+
+go 1.19
+
+require (
+	github.com/davidbyttow/govips/v2 v2.11.0
+	github.com/gernest/alien v0.1.0
+	github.com/google/uuid v1.3.0
+)
+
+require (
+	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5 // indirect
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+	golang.org/x/text v0.3.4 // indirect
+)
